8e2f09964509dbd5b1511b7486ad7631321c34ae
manonverra <manonverra@gmail.com>
2017-01-19 16:02:27 +0100

    ?

---
+++ STIROefeningManon/AnalyzingGATESimulations/AnalyzingGATESimulations.py
@@ -0,0 +1,11 @@
+import sys
+import stir
+import stirextra
+import pylab
+import numpy as np
+import os
+import time
+import matplotlib.pyplot as plt
+from StirSupport import *
+from scipy.optimize import minimize
+from prompt_toolkit import input
