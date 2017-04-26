95144b092d7463cc43749260be489ca2b680cce2
manonverra <manonverra@gmail.com>
2017-04-26 15:00:30 +0200

    Gating finished

--- STIROefeningManon/MotionModelProjSpaceNoSTIR/GatingNoSTIR.py
+++ STIROefeningManon/MotionModelProjSpaceNoSTIR/GatingNoSTIR.py
@@ -8,20 +8,20 @@ import scipy as sp
 
 #_________________________CONFIGURATION_______________________________
 phantom = 'Shepp-Logan' 
-noise = False
+noise = True
 #noise = True
 #motion = 'Step' 
 motion = 'Sine'
-stationary = True 
+stationary = False 
 #stationary = False # Only possible for sinusoidal motion 
 
-nIt = 1
+nIt = 10
 trueShiftAmplitude = 15 # Kan niet alle waardes aannemen (niet alle shifts worden geprobeerd) + LET OP: kan niet groter zijn dan de lengte van het plaatje (kan de code niet aan) 
 trueOffset = 5
 numFigures = 0 
 duration = 60 # in seconds
 if (motion == 'Step'): nFrames = 2
-else: nFrames = 30
+else: nFrames = 10
 gating = True
 
 dir = './Figures/'
@@ -33,26 +33,20 @@ mf.write_Configuration(figSaveDir, phantom, noise, motion, stationary, nIt, true
 
 #_________________________MAKE PHANTOM_______________________________
 image2D = mf.make_Phantom(phantom, duration)
-plt.figure(), plt.title('Original image'), plt.imshow(image2D, cmap=plt.cm.Greys_r, interpolation = None, vmin = 0, vmax = np.max(image2D)), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_phantom.png'.format(numFigures, trueShiftAmplitude)), plt.close()
+plt.figure(), plt.title('Original image'), plt.imshow(image2D, interpolation = None, vmin = 0, vmax = np.max(image2D)), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_phantom.png'.format(numFigures, trueShiftAmplitude)), plt.close()
 numFigures += 1
 
 
 #_________________________ADD MOTION_______________________________ 
 nonGatedPhantomList, nonGatedSurSignal, nonGatedShiftList = mf.move_Phantom(motion, nFrames, trueShiftAmplitude, trueOffset, image2D, stationary)
 
+
+#_________________________GATING_______________________________ 
 maxSurSignal = np.max(nonGatedSurSignal) 
 minSurSignal = np.min(nonGatedSurSignal)
 gateMin = minSurSignal
 gateMax = minSurSignal + 0.35*(maxSurSignal - minSurSignal)
-
-surSignal = []
-phantomList = []
-shiftList = []
-for i in range(len(nonGatedSurSignal)): 
-    if ((nonGatedSurSignal[i] <= gateMax) and (nonGatedSurSignal[i] >= gateMin)): 
-        surSignal.append(nonGatedSurSignal[i])
-        phantomList.append(nonGatedPhantomList[i])
-        shiftList.append(nonGatedShiftList[i])
+surSignal, phantomList, shiftList = mf.gating(nonGatedSurSignal, nonGatedPhantomList, nonGatedShiftList, gateMin, gateMax)
 
 # Visualization of gating 
 x = np.arange(0, len(nonGatedPhantomList), 0.1)
@@ -78,18 +72,15 @@ for iFrame in range(len(phantomList)):
     if (iFrame == 0): measWithNoise = meas
     measList.append(meas) 
 
-plt.subplot(1,2,1), plt.title('Without noise'), plt.imshow(measNoNoise, cmap=plt.cm.Greys_r, interpolation=None, vmin = 0, vmax = 1000)
-plt.subplot(1,2,2), plt.title('With noise'), plt.imshow(measWithNoise, cmap=plt.cm.Greys_r, interpolation=None, vmin = 0, vmax = 1000)
+plt.subplot(1,2,1), plt.title('Without noise'), plt.imshow(measNoNoise, interpolation=None, vmin = 0, vmax = 1000)
+plt.subplot(1,2,2), plt.title('With noise'), plt.imshow(measWithNoise, interpolation=None, vmin = 0, vmax = 1000)
 plt.suptitle('Time Frame 1'), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_measurementsWithWithoutNoise.png'.format(numFigures, trueShiftAmplitude)), plt.close()
 numFigures += 1 
 
 
 #_________________________INITIAL GUESS_______________________________
-reconList = []
-for iFrame in range(len(measList)): 
-    reconList.append(iradon(measList[iFrame], iAngles)) 
-guess = np.mean(reconList, axis = 0)
-plt.figure(), plt.title('Initial guess'), plt.imshow(guess, cmap=plt.cm.Greys_r, interpolation = None, vmin = 0, vmax = np.max(image2D)), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_InitialGuess.png'.format(numFigures, trueShiftAmplitude)), plt.close() 
+guess = np.ones(np.shape(phantomList[0]))[0,:,:]
+plt.figure(), plt.title('Initial guess'), plt.imshow(guess, interpolation = None, vmin = 0, vmax = np.max(image2D)), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_InitialGuess.png'.format(numFigures, trueShiftAmplitude)), plt.close() 
 numFigures += 1
 
 
@@ -99,28 +90,22 @@ norm = iradon(normSino, iAngles, filter = None) # We willen nu geen ramp filter
 
 
 #_________________________NESTED EM LOOP_______________________________
-guessSum = []
-guessSum.append(np.sum(guess))
 for iIt in range(nIt): 
     for iFrame in range(len(surSignal)): 
-        shiftedGuess = np.zeros(np.shape(guess))
-        shiftedGuess = guess 
-        shiftedGuessSinogram = radon(shiftedGuess, iAngles) 
-        error = measList[iFrame]/shiftedGuessSinogram 
+        guessSinogram = radon(guess, iAngles)
+        error = measList[iFrame]/guessSinogram 
         error[np.isnan(error)] = 0
         error[np.isinf(error)] = 0
         error[error > 1E10] = 0;
         error[error < 1E-10] = 0
         errorBck = iradon(error, iAngles, filter = None) 
-        errorBckShifted = errorBck
-        guess *= errorBckShifted
+        guess *= errorBck 
     guess /= norm 
-    guessSum.append(np.sum(guess))
     countIt = iIt+1 
 
-    plt.figure(), plt.title('Guess after {0} iteration(s)'.format(iIt+1)), plt.imshow(guess, cmap=plt.cm.Greys_r, interpolation = None, vmin = 0, vmax = np.max(image2D)), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_finalImage.png'.format(numFigures, trueShiftAmplitude)), plt.close()
+    plt.figure(), plt.title('Guess after {0} iteration(s)'.format(iIt+1)), plt.imshow(guess, interpolation = None, vmin = 0, vmax = np.max(image2D)), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_finalImage.png'.format(numFigures, trueShiftAmplitude)), plt.close()
     numFigures += 1  
 
-plt.figure(), plt.subplot(1,2,1), plt.title('Original Image'), plt.imshow(image2D, cmap=plt.cm.Greys_r, interpolation=None, vmin = 0, vmax = np.max(image2D))
-plt.subplot(1,2,2), plt.title('Reconstructed Image'), plt.imshow(guess, cmap=plt.cm.Greys_r, interpolation=None, vmin = 0, vmax = np.max(image2D)), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_originalAndRecon.png'.format(numFigures, trueShiftAmplitude)), plt.close() 
+plt.figure(), plt.subplot(1,2,1), plt.title('Original Image'), plt.imshow(image2D, interpolation=None, vmin = 0, vmax = np.max(image2D))
+plt.subplot(1,2,2), plt.title('Reconstructed Image'), plt.imshow(guess, interpolation=None, vmin = 0, vmax = np.max(image2D)), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_originalAndRecon.png'.format(numFigures, trueShiftAmplitude)), plt.close() 
 numFigures += 1 
