f8991430613c67ba4889a4e3d3de4ea4812f4060
manonverra <manonverra@gmail.com>
2017-04-28 11:19:58 +0200

    Revert "Removed old code"
    This reverts commit af846aa225b8859d47c3e7acb0df6d1a86eb19a5.

--- STIROefeningManon/MotionModelProjSpaceNoSTIR/MotionModelProjSpaceNoSTIR.py
+++ STIROefeningManon/MotionModelProjSpaceNoSTIR/MotionModelProjSpaceNoSTIR.py
@@ -5,8 +5,7 @@ from skimage.transform import iradon, radon
 import ManonsFunctions as mf 
 import scipy as sp
 
-
-#_________________________CONFIGURATION_______________________________
+#phantom = 'Block'
 phantom = 'Shepp-Logan' 
 noise = False
 #noise = True
@@ -15,7 +14,7 @@ motion = 'Sine'
 stationary = True 
 #stationary = False # Only possible for sinusoidal motion 
 
-nIt = 10
+nIt = 30
 trueShiftAmplitude = 15 # Kan niet alle waardes aannemen (niet alle shifts worden geprobeerd) + LET OP: kan niet groter zijn dan de lengte van het plaatje (kan de code niet aan) 
 trueOffset = 5
 numFigures = 0 
@@ -28,13 +27,11 @@ figSaveDir = mf.make_figSaveDir(dir, motion, phantom, noise, stationary)
 
 mf.write_Configuration(figSaveDir, phantom, noise, motion, stationary, nIt, trueShiftAmplitude, trueOffset, duration, nFrames)
 
-
 #_________________________MAKE PHANTOM_______________________________
 image2D = mf.make_Phantom(phantom, duration)
 plt.figure(), plt.title('Original image'), plt.imshow(image2D, interpolation = None, vmin = 0, vmax = np.max(image2D)), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_phantom.png'.format(numFigures, trueShiftAmplitude)), plt.close()
 numFigures += 1
  
-
 #_________________________ADD MOTION_______________________________ 
 phantomList, surSignal, shiftList = mf.move_Phantom(motion, nFrames, trueShiftAmplitude, trueOffset, image2D, stationary)
 originalImage = phantomList[0]
@@ -47,7 +44,6 @@ numFigures += 1
 plt.plot(range(nFrames), surSignal, label = 'Surrogate signal'), plt.title('Sinusoidal phantom shifts'), plt.xlabel('Time frame'), plt.ylabel('Shift')
 plt.plot(range(nFrames), shiftList, label = 'True motion'), plt.legend(loc = 4), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_shiftList.png'.format(numFigures, trueShiftAmplitude)), plt.close()
 numFigures += 1 
-
 
 #_________________________MEASUREMENT, INITIAL GUESS, NORMALIZATION_______________________________
 iAngles = np.linspace(0, 360, 120)[:-1]
@@ -69,25 +65,25 @@ plt.subplot(1,2,2), plt.title('With noise'), plt.imshow(measWithNoise, interpola
 plt.suptitle('Time Frame 1'), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_measurementsWithWithoutNoise.png'.format(numFigures, trueShiftAmplitude)), plt.close()
 numFigures += 1 
 
-
-#_________________________INITIAL GUESS_______________________________
 reconList = []
 for iFrame in range(len(measList)): 
+
     reconList.append(iradon(measList[iFrame], iAngles)) 
 guess = np.mean(reconList, axis = 0)
+#guess = reconList[0] # Added
 plt.figure(), plt.title('Initial guess'), plt.imshow(guess, interpolation = None, vmin = 0, vmax = np.max(image2D)), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_InitialGuess.png'.format(numFigures, trueShiftAmplitude)), plt.close()
 numFigures += 1 
 
-
-#_________________________NORMALIZATION_______________________________
 normSino = np.ones(np.shape(measList[0]))
-norm = iradon(normSino, iAngles, filter = None) # We willen nu geen ramp filter 
-
+norm = iradon(normSino, iAngles, filter = None) # We willen nu geen ramp filter
+plt.figure(), plt.title('MLEM normalization'), plt.imshow(norm, interpolation = None, vmin = 0, vmax = 0.03), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_norm.png'.format(numFigures, trueShiftAmplitude)), plt.close()
+numFigures += 1  
 
 #_________________________NESTED EM LOOP_______________________________
 offsetFoundList = []
 quadErrorSumFoundList = []
 quadErrorSumListList = []
+#offsetFound = trueOffset-2 # First guess 
 guessSum = []
 guessSum.append(np.sum(guess))
 for iIt in range(nIt): 
