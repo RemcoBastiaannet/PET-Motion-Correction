8564966b6dae289dad2ee2907aab9f442f741b2d
manonverra <manonverra@gmail.com>
2017-05-17 17:43:40 +0200

    Bug with stripes fixed (due to filter in initial guess)

--- STIROefeningManon/MotionModelProjSpaceNoSTIR/MotionModelProjSpaceNoSTIR.py
+++ STIROefeningManon/MotionModelProjSpaceNoSTIR/MotionModelProjSpaceNoSTIR.py
@@ -4,6 +4,8 @@ import numpy as np
 from skimage.transform import iradon, radon
 import ManonsFunctions as mf 
 import scipy as sp
+import pyvpx
+import copy
 
 #phantom = 'Block'
 phantom = 'Shepp-Logan' 
@@ -14,13 +16,13 @@ motion = 'Sine'
 stationary = True 
 #stationary = False # Only possible for sinusoidal motion 
 
-nIt = 10
+nIt = 5
 trueShiftAmplitude = 15 # Kan niet alle waardes aannemen (niet alle shifts worden geprobeerd) + LET OP: kan niet groter zijn dan de lengte van het plaatje (kan de code niet aan) 
 trueOffset = 5
 numFigures = 0 
 duration = 60 # in seconds
 if (motion == 'Step'): nFrames = 2
-else: nFrames = 40
+else: nFrames = 10
 noiseLevel = 10 
 gating = False 
 
@@ -31,15 +33,18 @@ mf.write_Configuration(figSaveDir, phantom, noise, motion, stationary, nIt, true
 
 #_________________________MAKE PHANTOM_______________________________
 image2D = mf.make_Phantom(phantom, duration, noiseLevel)
-plt.figure(), plt.title('Original image'), plt.imshow(image2D, interpolation = None, vmin = 0, vmax = np.max(image2D)), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_phantom.png'.format(numFigures, trueShiftAmplitude)), plt.close()
+plt.figure(), plt.title('Original image'), plt.imshow(image2D, interpolation = None, vmin = 0, vmax = np.max(image2D), cmap=plt.cm.Greys_r), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_phantom.png'.format(numFigures, trueShiftAmplitude)), plt.close()
 numFigures += 1
+image2DTMP = np.zeros((1, 460, 460))
+image2DTMP[0,:,:] = image2D
+pyvpx.numpy2vpx(image2DTMP, figSaveDir + 'image2D.vpx') 
  
 #_________________________ADD MOTION_______________________________ 
 phantomList, surSignal, shiftList = mf.move_Phantom(motion, nFrames, trueShiftAmplitude, trueOffset, image2D, stationary)
 originalImage = phantomList[0]
 
 for iFrame in range(nFrames):    
-    plt.subplot(2,nFrames/2+1,iFrame+1), plt.title('Time frame {0}'.format(iFrame)), plt.imshow(phantomList[iFrame][0,:,:], interpolation=None, vmin = 0, vmax = np.max(image2D)) 
+    plt.subplot(2,nFrames/2+1,iFrame+1), plt.title('Time frame {0}'.format(iFrame)), plt.imshow(phantomList[iFrame][0,:,:], interpolation=None, vmin = 0, vmax = np.max(image2D), cmap=plt.cm.Greys_r) 
 plt.suptitle('Phantom'), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_phantom.png'.format(numFigures, trueShiftAmplitude)), plt.close()
 numFigures += 1 
  
@@ -52,33 +57,46 @@ iAngles = np.linspace(0, 360, 120)[:-1]
 
 measList = []
 for iFrame in range(nFrames):
-    meas = radon(phantomList[iFrame][0,:,:], iAngles) 
+    meas = radon(copy.deepcopy(phantomList[iFrame])[0,:,:], iAngles) 
     if (iFrame == 0): measNoNoise = meas
     if (noise): 
         meas = sp.random.poisson(meas)
     if (iFrame == 0): measWithNoise = meas
-    plt.subplot(2,nFrames/2+1,iFrame+1), plt.title('Time frame {0}'.format(iFrame)), plt.imshow(meas, interpolation=None, vmin = 0, vmax = 1000) 
+    plt.subplot(2,nFrames/2+1,iFrame+1), plt.title('Time frame {0}'.format(iFrame)), plt.imshow(meas, interpolation=None, vmin = 0, vmax = 1000, cmap=plt.cm.Greys_r) 
     measList.append(meas) 
+    measTMP = np.zeros((1, 651, 119))
+    measTMP[0,:,:] = measList[iFrame]
+    pyvpx.numpy2vpx(measTMP, figSaveDir + 'meas_{}.vpx'.format(iFrame)) 
 plt.suptitle('Measurements'), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_measurements.png'.format(numFigures, trueShiftAmplitude)), plt.close()
 numFigures += 1 
 
-plt.subplot(1,2,1), plt.title('Without noise'), plt.imshow(measNoNoise, interpolation=None, vmin = 0, vmax = noiseLevel)
-plt.subplot(1,2,2), plt.title('With noise'), plt.imshow(measWithNoise, interpolation=None, vmin = 0, vmax = noiseLevel)
+plt.figure() 
+plt.subplot(1,2,1), plt.title('Without noise'), plt.imshow(measNoNoise, interpolation=None, vmin = 0, vmax = noiseLevel, cmap=plt.cm.Greys_r)
+plt.subplot(1,2,2), plt.title('With noise'), plt.imshow(measWithNoise, interpolation=None, vmin = 0, vmax = noiseLevel, cmap=plt.cm.Greys_r)
 plt.suptitle('Time Frame 1'), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_measurementsWithWithoutNoise.png'.format(numFigures, trueShiftAmplitude)), plt.close()
 numFigures += 1 
 
 reconList = []
 for iFrame in range(len(measList)): 
-    reconList.append(iradon(measList[iFrame], iAngles, filter = None)) 
+    reconList.append(iradon(copy.deepcopy(measList[iFrame]), iAngles, filter = None)) 
+    reconTMP = np.zeros((1, 460, 460))
+    reconTMP[0,:,:] = reconList[iFrame]
+    pyvpx.numpy2vpx(reconTMP, figSaveDir + 'reconGuess_{}.vpx'.format(iFrame)) 
 guess = np.mean(reconList, axis = 0)
 #guess = reconList[0] # Added
-plt.figure(), plt.title('Initial guess'), plt.imshow(guess, interpolation = None, vmin = 0, vmax = np.max(image2D)), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_InitialGuess.png'.format(numFigures, trueShiftAmplitude)), plt.close()
+plt.figure(), plt.title('Initial guess'), plt.imshow(guess, interpolation = None, vmin = 0, vmax = np.max(guess), cmap=plt.cm.Greys_r), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_InitialGuess.png'.format(numFigures, trueShiftAmplitude)), plt.close()
 numFigures += 1 
+guessTMP = np.zeros((1, 460, 460))
+guessTMP[0,:,:] = guess
+pyvpx.numpy2vpx(guessTMP, figSaveDir + 'guess.vpx') 
 
 normSino = np.ones(np.shape(measList[0]))
 norm = iradon(normSino, iAngles, filter = None) # We willen nu geen ramp filter
-plt.figure(), plt.title('MLEM normalization'), plt.imshow(norm, interpolation = None, vmin = 0, vmax = 0.03), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_norm.png'.format(numFigures, trueShiftAmplitude)), plt.close()
+plt.figure(), plt.title('MLEM normalization'), plt.imshow(norm, interpolation = None, vmin = 0, vmax = 0.03, cmap=plt.cm.Greys_r), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_norm.png'.format(numFigures, trueShiftAmplitude)), plt.close()
 numFigures += 1  
+normTMP = np.zeros((1, 460, 460))
+normTMP[0,:,:] = norm
+pyvpx.numpy2vpx(normTMP, figSaveDir + 'norm.vpx') 
 
 #_________________________NESTED EM LOOP_______________________________
 offsetFoundList = []
@@ -87,12 +105,39 @@ quadErrorSumListList = []
 #offsetFound = trueOffset-2 # First guess 
 guessSum = []
 guessSum.append(np.sum(guess))
+offsetFound = 0 # first guess 
 for iIt in range(nIt): 
+    # Normal MLEM 
+    for iFrame in range(nFrames): 
+        shiftedGuess = np.zeros(np.shape(guess))
+        shiftedGuess = guess
+        sp.ndimage.shift(guess, (surSignal[iFrame] - offsetFound, 0), shiftedGuess)
+        #shiftedGuessSinogram = radon(shiftedGuess, iAngles) 
+        error = measList[iFrame]/shiftedGuessSinogram 
+        error[np.isnan(error)] = 0
+        error[np.isinf(error)] = 0
+        error[error > 1E10] = 0;
+        error[error < 1E-10] = 0
+        errorBck = iradon(error, iAngles, filter = None) 
+        errorBckShifted = np.zeros(np.shape(errorBck))
+        sp.ndimage.shift(errorBck, (-surSignal[iFrame] + offsetFound, 0), errorBckShifted)
+        #errorBckShifted = errorBck
+        guess *= errorBckShifted
+    guess /= norm 
+    guessTMP = np.zeros((1, 460, 460))
+    guessTMP[0,:,:] = guess
+    pyvpx.numpy2vpx(guessTMP, figSaveDir + 'guess_{}.vpx'.format(iIt)) 
+    guessSum.append(np.sum(guess))
+    countIt = iIt+1 
+
+    plt.figure(), plt.title('Guess after {0} iteration(s)'.format(iIt+1)), plt.imshow(guess, interpolation = None, vmin = 0, vmax = np.max(image2D), cmap=plt.cm.Greys_r), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_finalImage.png'.format(numFigures, trueShiftAmplitude)), plt.close()
+    numFigures += 1  
+
     # Motion model optimization
     guessMovedList = []
     guessMovedProjList = []
     quadErrorSumList = []   
-    offsetList = range(trueOffset-3, trueOffset+4)
+    offsetList = range(-trueOffset-3, trueOffset+4)
     for offset in offsetList: 
         quadErrorSum = 0 
         for iFrame in range(nFrames): 
@@ -118,33 +163,11 @@ for iIt in range(nIt):
     numFigures += 1 
     plt.close()
 
-    # Normal MLEM 
-    for iFrame in range(nFrames): 
-        shiftedGuess = np.zeros(np.shape(guess))
-        #shiftedGuess = guess
-        sp.ndimage.shift(guess, (surSignal[iFrame] - offsetFound, 0), shiftedGuess)
-        shiftedGuessSinogram = radon(shiftedGuess, iAngles) 
-        error = measList[iFrame]/shiftedGuessSinogram 
-        error[np.isnan(error)] = 0
-        error[np.isinf(error)] = 0
-        error[error > 1E10] = 0;
-        error[error < 1E-10] = 0
-        errorBck = iradon(error, iAngles, filter = None) 
-        errorBckShifted = np.zeros(np.shape(errorBck))
-        sp.ndimage.shift(errorBck, (-surSignal[iFrame] + offsetFound, 0), errorBckShifted)
-        #errorBckShifted = errorBck
-        guess *= errorBckShifted
-    guess /= norm 
-    guessSum.append(np.sum(guess))
-    countIt = iIt+1 
-
-    plt.figure(), plt.title('Guess after {0} iteration(s)'.format(iIt+1)), plt.imshow(guess, interpolation = None, vmin = 0, vmax = np.max(image2D)), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_finalImage.png'.format(numFigures, trueShiftAmplitude)), plt.close()
-    numFigures += 1  
-
-plt.figure(), plt.subplot(1,2,1), plt.title('Original Image'), plt.imshow(originalImage[0,:,:], interpolation=None, vmin = 0, vmax = np.max(image2D))
-plt.subplot(1,2,2), plt.title('Reconstructed Image'), plt.imshow(guess, interpolation=None, vmin = 0, vmax = np.max(image2D)), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_originalAndRecon.png'.format(numFigures, trueShiftAmplitude)), plt.close() 
+plt.figure(), plt.subplot(1,2,1), plt.title('Original Image'), plt.imshow(originalImage[0,:,:], interpolation=None, vmin = 0, vmax = np.max(image2D), cmap=plt.cm.Greys_r)
+plt.subplot(1,2,2), plt.title('Reconstructed Image'), plt.imshow(guess, interpolation=None, vmin = 0, vmax = np.max(image2D), cmap=plt.cm.Greys_r), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_originalAndRecon.png'.format(numFigures, trueShiftAmplitude)), plt.close() 
 numFigures += 1 
 
+plt.figure() 
 plt.plot(guessSum), plt.title('Sum of guess'), plt.savefig(figSaveDir + 'Fig{}_TrueShift{}_guessSum.png'.format(numFigures, trueShiftAmplitude))
 numFigures += 1 
 plt.close() 
