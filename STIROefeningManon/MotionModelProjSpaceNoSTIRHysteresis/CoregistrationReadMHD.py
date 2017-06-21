586791b2046104f9bc5919e508e03443bec3fa46
manonverra <manonverra@gmail.com>
2017-06-21 16:05:08 +0200

    Gates are now averaged in python rather than slicer

--- STIROefeningManon/MotionModelProjSpaceNoSTIRHysteresis/CoregistrationReadMHD.py
+++ STIROefeningManon/MotionModelProjSpaceNoSTIRHysteresis/CoregistrationReadMHD.py
@@ -2,22 +2,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 import ManonsFunctionsHysteresis as mf
 
-filePath = "E:/Manon/Resultaten_Simulaties/4_Coregistratie/Stationair/"
-
+stationary = True 
+phantom = 'Shepp-Logan'
+noiseLevel = 600
 nGates = 5
-gatedImages = []
-for iGate in range(1, nGates+1):
-    gatedImages.append(mf.load_itk(filePath + "Seperate_gates_unregistered/Gate{}.mhd".format(iGate)))
-meanImageUnregistered = np.mean(gatedImages, axis = 0)
-plt.figure(), plt.title('Mean without registration'), plt.imshow(meanImageUnregistered[:,:,0], interpolation = None, vmin = 0, vmax = np.max(gatedImages[0]), cmap=plt.cm.Greys_r)
-plt.savefig(filePath + "MeanWithoutRegistration.png")
-plt.close()
 
-meanImageRegistered = mf.load_itk(filePath + "Slicer_output_registered/RegisteredVolume.mhd")
-meanImageRegistered /= 5
-plt.figure(), plt.title('Mean with registration'), plt.imshow(meanImageRegistered[:,:,0], interpolation = None, vmin = 0, vmax = np.max(gatedImages[0]), cmap=plt.cm.Greys_r)
+image2D = mf.make_Phantom(phantom, noiseLevel)
+
+filePath = "E:/Manon/Resultaten_Simulaties/4_Coregistratie/"
+
+if (stationary): filePath += "Stationair/"
+else: filePath += "Niet-stationair/"
+
+gatedImagesReg = []
+for iGate in range(1, nGates+1):
+    gatedImagesReg.append(mf.load_itk(filePath + "Elastix_output/Gate{}.mhd".format(iGate, iGate)))
+meanImageRegistered = np.mean(gatedImagesReg, axis = 0)
+plt.figure(), plt.title('Mean with registration'), plt.imshow(meanImageRegistered[:,:,0], interpolation = None, vmin = 0, vmax = np.max(gatedImagesReg[0]), cmap=plt.cm.Greys_r)
 plt.savefig(filePath + "MeanWithRegistration.png")
 plt.close()
 
-mf.writeMhdFile(meanImageUnregistered, filePath + "MeanUnregistered.mhd")
 mf.writeMhdFile(meanImageRegistered, filePath + "MeanRegistered.mhd")
+
+qualityFile = open(filePath + "Quality.txt", "w")
+qualityFile.write('Simulations (registered):\n')
+qualityFile.write('Maximum value: {}\n'.format(np.max(meanImageRegistered))) 
+qualityFile.write('Quadratic difference of simulation and phantom: {}\n'.format(np.sum( (meanImageRegistered[:,:,0] - image2D)**2 )))
+qualityFile.close() 
