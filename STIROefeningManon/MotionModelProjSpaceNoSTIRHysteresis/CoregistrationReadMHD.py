ae2957b32a1d729463476d0921d9c734d970867c
manonverra <manonverra@gmail.com>
2017-07-21 11:00:01 +0200

    Removed redundant code

--- STIROefeningManon/MotionModelProjSpaceNoSTIRHysteresis/CoregistrationReadMHD.py
+++ STIROefeningManon/MotionModelProjSpaceNoSTIRHysteresis/CoregistrationReadMHD.py
@@ -2,12 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import ManonsFunctions as mf
 
-stationary = True 
-phantom = 'Shepp-Logan'
-noiseLevel = 600
+stationary = False 
 nGates = 5
-
-image2D = mf.make_Phantom(phantom, noiseLevel)
 
 filePath = "E:/Manon/Resultaten_Simulaties/4_Coregistratie/"
 
@@ -16,16 +12,10 @@ else: filePath += "Niet-stationair/"
 
 gatedImagesReg = []
 for iGate in range(1, nGates+1):
-    gatedImagesReg.append(mf.load_itk(filePath + "Elastix_output/Gate{}.mhd".format(iGate, iGate)))
+    gatedImagesReg.append(mf.load_itk(filePath + "Coregistered_gates/Gate{}.mhd".format(iGate, iGate)))
 meanImageRegistered = np.mean(gatedImagesReg, axis = 0)
 plt.figure(), plt.title('Mean with registration'), plt.imshow(meanImageRegistered[:,:,0], interpolation = None, vmin = 0, vmax = np.max(gatedImagesReg[0]), cmap=plt.cm.Greys_r)
 plt.savefig(filePath + "MeanWithRegistration.png")
 plt.close()
 
 mf.writeMhdFile(meanImageRegistered, filePath + "MeanRegistered.mhd")
-
-qualityFile = open(filePath + "Quality.txt", "w")
-qualityFile.write('Simulations (registered):\n')
-qualityFile.write('Maximum value: {}\n'.format(np.max(meanImageRegistered))) 
-qualityFile.write('Quadratic difference of simulation and phantom: {}\n'.format(np.sum( (meanImageRegistered[:,:,0] - image2D)**2 )))
-qualityFile.close() 
