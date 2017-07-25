487efcd0fc5cf0794e9d30d00655cee2f3a2a444
manonverra <manonverra@gmail.com>
2017-07-25 17:19:10 +0200

    Dingetjes

--- STIROefeningManon/MotionModelProjSpaceNoSTIRHysteresis/CoregistrationReadMHD.py
+++ STIROefeningManon/MotionModelProjSpaceNoSTIRHysteresis/CoregistrationReadMHD.py
@@ -1,21 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import ManonsFunctions as mf
+import pyvpx
 
-stationary = False 
 nGates = 5
+experiment = 'Sinus2D/'
 
-filePath = "E:/Manon/Resultaten_Simulaties/4_Coregistratie/"
-
-if (stationary): filePath += "Stationair/"
-else: filePath += "Niet-stationair/"
+filePath = "E:/Manon/Resultaten_Experimenten/" + experiment + "Gating/"
 
 gatedImagesReg = []
 for iGate in range(1, nGates+1):
     gatedImagesReg.append(mf.load_itk(filePath + "Coregistered_gates/Gate{}.mhd".format(iGate, iGate)))
 meanImageRegistered = np.mean(gatedImagesReg, axis = 0)
-plt.figure(), plt.axis('off'), plt.title('Mean with registration'), plt.imshow(meanImageRegistered[:,:,0], interpolation = None, vmin = 0, vmax = np.max(gatedImagesReg[0]), cmap=plt.cm.Greys_r)
-plt.savefig(filePath + "MeanWithRegistration.png")
-plt.close()
 
+pyvpx.numpy2vpx(meanImageRegistered, filePath + "MeanRegistered.vpx")
 mf.writeMhdFile(meanImageRegistered, filePath + "MeanRegistered.mhd")
